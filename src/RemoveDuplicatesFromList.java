c4216ad759472e44a03e153a7de5d9e1d6f63eba
ashutosh7131 <ashutosh713@outlook.com>
2023-12-24 01:29:45 +0800

    Java program to remove all duplicate elements from a list using streams.

---
+++ src/RemoveDuplicatesFromList.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RemoveDuplicatesFromList {
+
+    public static void main(String[] args) {
+
+        List <Integer> nums = Arrays.asList(10, 23, 22, 23, 24, 24, 33, 15, 26, 15);
+        System.out.println("Original List of numbers: " + nums);
+        List<Integer> distinctList = nums.stream().distinct().sorted().toList();
+        System.out.println("Distinct List of numbers: " + distinctList);
+
+
+
+    }
+}
