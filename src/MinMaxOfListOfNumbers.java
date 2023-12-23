861997cc21ad22fd3a7a9e8aa6e430f58b75664a
ashutosh7131 <ashutosh713@outlook.com>
2023-12-24 01:49:41 +0800

    Java program to find the maximum and minimum values in a list of integers using streams.

---
+++ src/MinMaxOfListOfNumbers.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class MinMaxOfListOfNumbers {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbs = Arrays.asList(1, 17, 54, 14, 14, 33, 45, -11);
+        System.out.println("Original list of numbers: " + numbs);
+        Integer max_Value = numbs.stream().max(Integer::compare).orElse(null);
+        System.out.println("Printing Max Number ::: " + max_Value);
+        Integer min_Value = numbs.stream().min(Integer::compare).orElse(null);
+        System.out.println("Printing Max Number ::: " + min_Value);
+
+    }
+}
