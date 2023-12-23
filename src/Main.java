4b04f4b854e994826db38603e156cd3b269b4b45
ashutosh7131 <ashutosh713@outlook.com>
2023-12-23 20:19:13 +0800

    Filter Map based on value computer and sort the keys on basis of that and print the o/p

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        Map<Integer, String> map = new HashMap<Integer, String>();
+        Map.Entry<Integer, String> result2 ;
+        map.put(1, "Computer");
+        map.put(2, "Mouse");
+        map.put(8, "Computer");
+        map.put(4, "Trackpad");
+        map.put(6, "Mouse");
+        map.put(5, "Computer");
+
+        map.entrySet().stream().filter(x->x.getValue().contains("Computer")).forEach(System.out::println);
+        map.entrySet().stream().filter(entry -> entry.getValue().contains("Computer")).map(Map.Entry::getKey).forEach(System.out::println);
+
+
+
+
+
+        }
+    }
