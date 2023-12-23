8c9db5a9b64286877f60133e205f911d16c99cf1
ashutosh7131 <ashutosh713@outlook.com>
2023-12-24 01:10:35 +0800

    name update commit for map filter

--- src/FilterAndSortMapBasedOnValue.java
+++ src/FilterAndSortMapBasedOnValue.java
@@ -1,9 +1,8 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class FilterAndSortMapBasedOnValue {
     public static void main(String[] args) {
 
         Map<Integer, String> map = new HashMap<Integer, String>();
